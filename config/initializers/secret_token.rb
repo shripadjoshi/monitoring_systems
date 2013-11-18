f2288f47885446377555af4e041971885919aba6
Shripad <sjoshi@etouch.net>
2013-11-18 05:23:15 +0000

    application commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MonitoringSystems::Application.config.secret_key_base = 'b3a5f2e40476966afc989b4f8d03f11b3601f73a85d02b8d6cc1304c399123ff0dfc99d0e6db6dc6e84d5c8f0ef5570d73522fd7a39c9138c506b210a07975d6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MonitoringSystems::Application.config.session_store :cookie_store, key: '_monitoring_systems_session'
